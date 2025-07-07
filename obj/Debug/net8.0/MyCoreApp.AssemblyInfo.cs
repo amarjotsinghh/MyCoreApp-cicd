93cfd298d4e97f4e395bdb4927bd173e8cf12ef7
amarjotsinghh <amarjotsingh5555@gmail.com>
2025-07-07 13:35:50 +0530

    dotnet-ci workflow added

--- obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85b30c710eea282174f5cfe572a1fdd3987cebdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d473ac25f70a023465b04cdfe42b1e1bcf7ab046")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
