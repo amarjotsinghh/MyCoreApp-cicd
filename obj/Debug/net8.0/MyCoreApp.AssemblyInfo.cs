77894fe047d31ba1bb009f0f8a775ecd402d0c09
amarjotsinghh <amarjotsingh5555@gmail.com>
2025-07-08 10:54:51 +0530

    deploy to azure yaml added

--- obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c51ab585b90aeda10667212c49820a2973d12dcc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a811a53a07535fba8f226daaa434287563a3f19")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
